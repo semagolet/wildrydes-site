b078c57f76f840ce7885fe1982d1a9a590a54fe2
Semanur <46786005+semagolet@users.noreply.github.com>
2025-12-12 22:10:25 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_7Eiihz5Xc', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '782d6e78scj5m8mupidatjadlt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-eaast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
